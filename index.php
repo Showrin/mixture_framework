4a56ad565364da95d6c1ca6942eb3014d460cf8a
Showrin <showrinece15@gmail.com>
2018-09-11 06:52:37 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Mixture framework</title>
+
+	<link rel="stylesheet" type="text/css" href="css/mixture.css">
+	
+</head>
+<body>
+
+	
+
+
+
+
+
+	
+
+	<script type="text/javascript" src="js/mixture.js"></script>
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+
+</body>
+</html>
